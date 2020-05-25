5015b449c44d772f05e0a42485bd808a3be6e79d
nprindle <nprindle18@gmail.com>
2020-05-25 12:10:26 -0400

    Pin to more convenient nixpkgs

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -13,23 +13,9 @@ let
 
   # Package overrides
   packageOverlay = hself: hsuper: {
-    pandoc-citeproc = hsuper.callCabal2nix "pandoc-citeproc" (super.fetchFromGitHub {
-      owner = "jgm";
-      repo = "pandoc-citeproc";
-      rev = "a4a11ec91098ee355aa04cfdb00ec40dc4435c4f";
-      sha256 = "1dx540bdl2y2fmfygj6r5zl33hra2dlkr6kac9qf0y8rymwl97i3";
-    }) {};
-
     hakyll =
-      let
-        pkg = hsuper.callCabal2nix "hakyll" (super.fetchFromGitHub {
-          owner = "jaspervdj";
-          repo = "hakyll";
-          rev = "e97ea3afcc1779fd1a9967d8c175cdd33e0311bc";
-          sha256 = "0k1vikxdyq0npllr9jazv5k9g4hr1xik36c82n4cb2wvx7gbldhk";
-        }) {};
-        confFlags = [ "-f" "watchServer" "-f" "previewServer" ];
-      in hlib.appendConfigureFlags pkg confFlags;
+      let confFlags = [ "-f" "watchServer" "-f" "previewServer" ];
+      in hlib.appendConfigureFlags hsuper.hakyll confFlags;
   };
 
   # Result packages
